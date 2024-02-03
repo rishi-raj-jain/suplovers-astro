60da2a2acaa2abf6f5ec82a56234b329438848f5
Alejo <lawebdealejo@gmail.com>
2024-02-03 14:34:03 -0500

    adds site name to astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://suplovers.co",
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en"],
